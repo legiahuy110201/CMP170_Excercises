21ab3bd342513241d0452df454652dbf2eb7ca47
ADMIN <ADMIN@DESKTOP-UJ3H5CT>
2021-09-16 10:07:52 +0700

    push EX1

---
+++ EX1/EX1/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace EX1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string hoten;
+            Console.WriteLine("Please enter your name !");
+            hoten = Console.ReadLine();
+            Console.WriteLine("\nHello {0} !", hoten);
+        }
+    }
+}
